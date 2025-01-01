6b1b6873a2377cb9ac674b5c27f74b4547c5eace
Nandhakumar C <nandhakumar.chandrasekaran@wissen.com>
2025-01-01 17:07:39 +0530

    Pushing docker files to containarize influxdb

---
+++ docker/InfluxDB Docker/main.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Step 1: Run docker-compose to start the containers
+echo "Starting containers with docker-compose..."
+docker-compose up -d
+
+# Step 2: Wait for InfluxDB to initialize
+echo "Waiting for InfluxDB to initialize..."
+# Sleep time to wait for InfluxDB to start (adjust if needed)
+sleep 30
+
+# Step 3: Run the init.sh script
+echo "Running init.sh script..."
+bash ./scripts.sh
