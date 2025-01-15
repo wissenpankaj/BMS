5a6d96647624c29e55ffb44c8a069065a246bbb6
Puneeth-Babu <puneeth.babu@wissen.com>
2025-01-15 14:36:27 +0530

    Removed @RestController annotation from FaultLogController as it is no longer used

--- ReportingAPI/src/main/java/com/wissen/bms/reportingAPI/controller/FaultLogController.java
+++ ReportingAPI/src/main/java/com/wissen/bms/reportingAPI/controller/FaultLogController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.validation.annotation.Validated;
 
-@RestController
+//@RestController
 @RequestMapping("/api/faultlogs")
 @Validated
 public class FaultLogController {
